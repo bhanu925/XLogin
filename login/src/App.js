222320fb37a23f6800faf5858fbc3a1d13b85cf1
bhanu925 <bhanu.chintu925@gmail.com>
2024-05-06 21:53:31 +0530

    login-form

---
+++ login/src/App.js
@@ -0,0 +1,11 @@
+import Login from "./LoginComponent/Login";
+
+function App() {
+  return (
+    <div className="App">
+     <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ login/src/LoginComponent/Login.jsx
@@ -0,0 +1,41 @@
+import './Login.css';
+import { useState } from 'react';
+const Login = ()=>{
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [welcomeOn , setWelcomeOn ] = useState(false);
+
+    const handleSubmit =(event) =>{
+        event.preventDefault();
+        if(username==='user' && password==='password'){
+            setWelcomeOn(true);
+        }
+    }
+
+    return (
+        
+        <div className="login">
+            <h1>Login Page</h1>
+            {!welcomeOn?
+            (<form onSubmit={handleSubmit}>
+                <div>
+                   <label htmlFor="username">Username:</label>
+                    <input onChange={(e)=>setUsername(e.target.value)}
+                     type="text" id="username" name="username" 
+                     placeholder="username" required/>
+                </div>
+                <div>
+                   <label htmlFor="password">Password:</label>
+                    <input onChange={(e)=>setPassword(e.target.value)} 
+                    type="text" name="password" id="password" 
+                    placeholder="password" required/>
+                </div>
+                <input type="submit" />
+            </form>):(
+                <p>Welcome, {username}!</p>
+            )
+            }       
+        </div>
+    )
+}
+export default Login;
